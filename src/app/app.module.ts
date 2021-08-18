43ef57c51d6328cf866aba031579ca4ab100f6c1
Fernando Parmezani dos Reis <tetris.freis@terceiros.mag.com.br>
2021-08-18 16:49:58 -0300

    update app module Http Client In memory database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from './in-memory-database';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [ AppComponent ],
+    imports: [ BrowserModule, AppRoutingModule, HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase) ],
+    providers: [],
+    bootstrap: [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {}
